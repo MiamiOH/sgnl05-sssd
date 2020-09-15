b2666c3144634b749b6d8d4e1280869f85e7a4b1
Bradley Koby <kobyb@miamioh.edu>
2020-09-15 09:04:37 -0400

    Fix cannot reassign error when before passed via sssd class

--- manifests/init.pp
+++ manifests/init.pp
@@ -145,9 +145,9 @@ class sssd (
 
   if ! empty($service_dependencies) {
     if $mkhomedir and $manage_oddjobd {
-      $before = 'Service[oddjobd]'
+      $_before = ['Service[oddjobd]'] << $before
     } else {
-      $before = undef
+      $_before = $before
     }
 
     ensure_resource('service', $service_dependencies,
@@ -156,7 +156,7 @@ class sssd (
         hasstatus  => true,
         hasrestart => true,
         enable     => $service_enable,
-        before     => $before,
+        before     => $_before,
       }
     )
   }
